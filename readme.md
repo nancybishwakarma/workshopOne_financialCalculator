32dfb420fb460723c6bde2c46d3c54e18a35b976
Nancy Bishwakarma <nancy.bishwakarma@gmail.com>
2025-04-13 15:42:37 -0400

    readme

--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
-# financial calculator
+# Workshop 1
+
+# Interesting code:
+scanner.nextLine()
+
+I had to use this code otherwise it was not taking my string input. I found out that if I'm inputting different data type then a string. I need a make a space in between them. scanner.nextLine() made that space and i was able to input my string without the code abruptly ending. 
+
+![](C:\Users\Student\Desktop\pluralSight\workshops\workshopOne_financialCalculator\Screenshots\Homescreen.png)
--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -4,72 +4,72 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
-        System.out.println("| Welcome to PluralSight Bank's Mortgage Calculator |");
-        System.out.println("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
-        System.out.println("Please enter the total amount of your loan");
-        double principalAmount = scanner.nextDouble();
-        System.out.println("Please enter the annual interest rate for your loan (in percentage)");
-        float interestRate = scanner.nextFloat();
-        System.out.println("Please enter the length of the loan (in years)");
-        int loanLength = scanner.nextInt();
+//        System.out.println("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
+//        System.out.println("| Welcome to PluralSight Bank's Mortgage Calculator |");
+//        System.out.println("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
+//        System.out.println("Please enter the total amount of your loan");
+//        double principalAmount = scanner.nextDouble();
+//        System.out.println("Please enter the annual interest rate for your loan (in percentage)");
+//        float interestRate = scanner.nextFloat();
+//        System.out.println("Please enter the length of the loan (in years)");
+//        int loanLength = scanner.nextInt();
+//
+//        System.out.println("Please confirm that the following information is correct:");
+//        System.out.println("Total amount of loan: "+ principalAmount);
+//        System.out.println("Annual Interest rate: " + interestRate);
+//        System.out.println("Loan length: " + loanLength);
+//        System.out.println("Enter 'Yes'  or 'No' ");
+//        scanner.nextLine();
+//        String infoConfirmation = scanner.nextLine();
+//
+//        if(infoConfirmation.equalsIgnoreCase("Yes")){
+//           float monthlyInterestRate = (interestRate/(100*12));
+//           int loanLengthInMonths = loanLength*12;
+//
+//           double monthlyPayment = principalAmount*((monthlyInterestRate* Math.pow(1+monthlyInterestRate,loanLengthInMonths))/(Math.pow(1+monthlyInterestRate,loanLengthInMonths)-1));
+//           double totalInterest = (monthlyPayment*loanLengthInMonths)-principalAmount;
+//            System.out.printf("Your monthly payment is: $%.2f\n",monthlyPayment);
+//
+//            System.out.printf("Your total interest is: $%.2f", totalInterest);
+//
+//        }
+//        else {
+//            System.out.println("Please restart again");
+//        }
 
-        System.out.println("Please confirm that the following information is correct:");
-        System.out.println("Total amount of loan: "+ principalAmount);
-        System.out.println("Annual Interest rate: " + interestRate);
-        System.out.println("Loan length: " + loanLength);
-        System.out.println("Enter 'Yes'  or 'No' ");
-        scanner.nextLine();
-        String infoConfirmation = scanner.nextLine();
 
-        if(infoConfirmation.equalsIgnoreCase("Yes")){
-           float monthlyInterestRate = (interestRate/(100*12));
-           int loanLengthInMonths = loanLength*12;
-
-           double monthlyPayment = principalAmount*((monthlyInterestRate* Math.pow(1+monthlyInterestRate,loanLengthInMonths))/(Math.pow(1+monthlyInterestRate,loanLengthInMonths)-1));
-           double totalInterest = (monthlyPayment*loanLengthInMonths)-principalAmount;
-            System.out.printf("Your monthly payment is: $%.2f\n",monthlyPayment);
-
-            System.out.printf("Your total interest is: $%.2f", totalInterest);
-
-        }
-        else {
-            System.out.println("Please restart again");
-        }
-
-
-        System.out.println("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
-     System.out.println("| Welcome to PluralSight Bank's Future Value Calculator |");
-       System.out.println("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
-        System.out.println("Please enter the total amount of your deposit");
-        double depositAmount = scanner.nextDouble();
-        System.out.println("Please enter the annual interest rate for your deposit (in percentage)");
-        float interestRate = scanner.nextFloat();
-        System.out.println("Please enter the length of the CD (in years)");
-        int cdLength = scanner.nextInt();
-
-        System.out.println("Please confirm that the following information is correct:");
-        System.out.println("Total amount of deposit: "+ depositAmount);
-        System.out.println("Annual Interest rate: " + interestRate);
-        System.out.println("CD length: " + cdLength);
-       System.out.println("Enter 'Yes'  or 'No' ");
-        scanner.nextLine();
-        String infoConfirmation = scanner.nextLine();
-
-        if(infoConfirmation.equalsIgnoreCase("Yes")){
-           float interestRateInDecimal = (interestRate/100);
-           int cdLengthInDays = cdLength*365;
-
-           double futureValue = depositAmount*Math.pow(1+(interestRateInDecimal/365),cdLengthInDays);
-           double totalInterest = futureValue-depositAmount;
-            System.out.printf("Your monthly payment is: $%.2f\n",futureValue);
-
-            System.out.printf("Your total interest is: $%.2f", totalInterest);
-
-        }
-        else {
-            System.out.println("Please restart again");
-        }
+//        System.out.println("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
+//     System.out.println("| Welcome to PluralSight Bank's Future Value Calculator |");
+//       System.out.println("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
+//        System.out.println("Please enter the total amount of your deposit");
+//        double depositAmount = scanner.nextDouble();
+//        System.out.println("Please enter the annual interest rate for your deposit (in percentage)");
+//        float interestRate = scanner.nextFloat();
+//        System.out.println("Please enter the length of the CD (in years)");
+//        int cdLength = scanner.nextInt();
+//
+//        System.out.println("Please confirm that the following information is correct:");
+//        System.out.println("Total amount of deposit: "+ depositAmount);
+//        System.out.println("Annual Interest rate: " + interestRate);
+//        System.out.println("CD length: " + cdLength);
+//       System.out.println("Enter 'Yes'  or 'No' ");
+//        scanner.nextLine();
+//        String infoConfirmation = scanner.nextLine();
+//
+//        if(infoConfirmation.equalsIgnoreCase("Yes")){
+//           float interestRateInDecimal = (interestRate/100);
+//           int cdLengthInDays = cdLength*365;
+//
+//           double futureValue = depositAmount*Math.pow(1+(interestRateInDecimal/365),cdLengthInDays);
+//           double totalInterest = futureValue-depositAmount;
+//            System.out.printf("Your monthly payment is: $%.2f\n",futureValue);
+//
+//            System.out.printf("Your total interest is: $%.2f", totalInterest);
+//
+//        }
+//        else {
+//            System.out.println("Please restart again");
+//        }
 
         System.out.println("|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|");
         System.out.println("| Welcome to PluralSight Bank's Present Value Calculator |");
